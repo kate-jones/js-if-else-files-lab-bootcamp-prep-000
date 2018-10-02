a8a274b49f56ed10fed3ae184e3ecca01fab98ea
kate.jones.h2o <kate.jones.h2o@gmail.com>
2018-10-02 02:35:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var name = "Joe"
+var name = "Susan"
 var height = "74"
 
 
@@ -11,5 +11,3 @@ if (name==='Susan') {
 } else {
   console.log("Expected the name to be Susan")
 }
-
-var name = "Susan"
